c8e8d0b7372fbf39c260ba5ef80196786674cba5
Mauricio Ortiz <ortiz.maurice@gmail.com>
2018-07-05 10:20:01 -0500

    boardPlace Class

--- gomoku.py
+++ gomoku.py
@@ -1,7 +1,13 @@
 from tkinter import *
 import random
 
+board =[]
 
+class boardPlace:
+	def __init__(self, x, y):
+		self.x =0
+		self.y = 0
+		self.value = 0
 
 
 def run(width = 500, height = 500):
