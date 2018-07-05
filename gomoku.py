a6a34e7dbfae887e577318ce4be9a13eaeaa7879
Mauricio Ortiz <ortiz.maurice@gmail.com>
2018-07-05 10:52:35 -0500

    Board generation

--- gomoku.py
+++ gomoku.py
@@ -5,18 +5,24 @@ board =[]
 
 class boardPlace:
 	def __init__(self, x, y, canvas):
-		self.x =0
-		self.y = 0
+		self.x = x
+		self.y = y
 		self.value = 0
+		self.visual = canvas.create_rectangle(self.x, self.y, self.x+30, self.y+30, fill = "grey", outline = "black", width = "1")
 		 
-def createBoard(canvas):
+def createBoard(canvas, x = 10, y = 40):
 	for row in range(19):
+		x = 10
+		y+=30
+		board.append([])
 		for col in range(19):
-			print(row, col)
+			x+=30
+			board[row].append(boardPlace(x, y, canvas))
 
-def run(width = 500, height = 500):
+def run(width = 650, height = 700):
 	def createBoardWrapper(canvas):
 		createBoard(canvas)
+		canvas.update()
 
 	root = Tk()
 	canvas = Canvas(root, width = width, height = height)
