c13ebe896eb839456163286692807ed83a130b20
Mauricio Ortiz <ortiz.maurice@gmail.com>
2018-07-05 10:24:34 -0500

    createBoard wrapper and outer function

--- gomoku.py
+++ gomoku.py
@@ -4,16 +4,26 @@ import random
 board =[]
 
 class boardPlace:
-	def __init__(self, x, y):
+	def __init__(self, x, y, canvas):
 		self.x =0
 		self.y = 0
 		self.value = 0
-
+		 
+def createBoard(canvas):
+	for row in range(19):
+		for col in range(19):
+			print(row, col)
 
 def run(width = 500, height = 500):
+	def createBoardWrapper(canvas):
+		createBoard(canvas)
+
 	root = Tk()
 	canvas = Canvas(root, width = width, height = height)
 	canvas.pack()
+
+	createBoardWrapper(canvas)
+
 	root.mainloop()
 
 run()
