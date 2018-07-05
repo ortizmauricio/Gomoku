1aa5e1c98d0fb47a7c6415da12c98ae8cd4a9b47
Mauricio Ortiz <ortiz.maurice@gmail.com>
2018-07-05 10:59:17 -0500

    Game session class and instance

--- gomoku.py
+++ gomoku.py
@@ -3,14 +3,23 @@ import random
 
 board =[]
 
+class Game:
+	def __init__(self):
+		self.player = 1
+		self.width = 650
+		self.height = 700
+
+session = Game()
+
+
 class boardPlace:
 	def __init__(self, x, y, canvas):
 		self.x = x
 		self.y = y
 		self.value = 0
 		self.visual = canvas.create_rectangle(self.x, self.y, self.x+30, self.y+30, fill = "grey", outline = "black", width = "1")
-		 
-def createBoard(canvas, x = 10, y = 40):
+
+def createBoard(canvas, x = ((session.width - 570)/2), y = 40):
 	for row in range(19):
 		x = 10
 		y+=30
@@ -19,7 +28,7 @@ def createBoard(canvas, x = 10, y = 40):
 			x+=30
 			board[row].append(boardPlace(x, y, canvas))
 
-def run(width = 650, height = 700):
+def run(width = session.width, height = session.height):
 	def createBoardWrapper(canvas):
 		createBoard(canvas)
 		canvas.update()
